367b5849ea37d412cbba53e382c2e923eb71ef59
Michael Demone <m_d_emone@hotmail.com>
2019-01-26 13:18:45 -0800

    Made the player animator and all of the animations.

---
+++ Assets/Code/LookAtStuff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtStuff : MonoBehaviour
+{
+
+    public Transform ThingToLookAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThingToLookAt);
+        transform.LookAt(new Vector3(transform.position.x, -7, transform.position.z));
+    }
+}
