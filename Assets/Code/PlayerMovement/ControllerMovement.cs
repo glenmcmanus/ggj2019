2386fa796859d21b5d2eccae6a9aa4befbfa1077
Michael Demone <m_d_emone@hotmail.com>
2019-01-26 11:53:22 -0800

    Added a new unity event and some controller movement but immediately removed it.

---
+++ Assets/Code/PlayerMovement/ControllerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+    public UnityEventVector2 OnControllerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Vector2 ControllerDir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((ControllerDir.x = Input.GetAxisRaw("Horizontal")) != 0 || (ControllerDir.y = Input.GetAxisRaw("Vertical")) != 0)
+        {
+            
+        }
+    }
+}
