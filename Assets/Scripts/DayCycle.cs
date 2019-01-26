35975c2b6670dad628b05cc3b0bc564b42a1b805
Glen <glen.don.mcmanus@gmail.com>
2019-01-26 15:58:10 -0800

    Add post processing; add periodic stam drain

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DayCycle : MonoBehaviour
+{
+    public Light light;
+    public Color daylight;
+    public Color moonlight;
+
+    [Tooltip("The duration of a day (in terms of daylight)")]
+    public float length = 180f;
+    [Tooltip("Duration between day-to-night update ticks")]
+    public float stepDelay = 1f;
+    WaitForSeconds delay;
+
+    public PostProcessProfile dayProfile;
+    Bloom dayBloom;
+    Vignette dayVignette;
+
+    public PostProcessProfile nightProfile;
+    Bloom nightBloom;
+    Vignette nightVignette;
+
+    public PostProcessVolume volume;
+    Bloom bloom;
+    Vignette vignette;
+
+    private void Awake()
+    {
+        dayBloom = dayProfile.GetSetting<Bloom>();
+        dayVignette = dayProfile.GetSetting<Vignette>();
+
+        nightBloom = nightProfile.GetSetting<Bloom>();
+        nightVignette = nightProfile.GetSetting<Vignette>();
+
+        bloom = volume.profile.GetSetting<Bloom>();
+        vignette = volume.profile.GetSetting<Vignette>();
+
+        delay = new WaitForSeconds(stepDelay);
+
+        StartCycle();
+    }
+
+    public void StartCycle()
+    {
+        light.color = daylight;
+
+        bloom.intensity.value = dayBloom.intensity.value;
+        bloom.color.value = dayBloom.color.value;
+        bloom.threshold.value = dayBloom.threshold.value;
+
+        vignette.intensity.value = dayVignette.intensity.value;
+        vignette.color.value = dayVignette.color.value;
+        vignette.smoothness.value = dayVignette.smoothness.value;
+
+        StartCoroutine(DayToNight());
+    }
+
+    public IEnumerator DayToNight()
+    {
+        float initTime = Time.time;
+        float curTime = Time.time - initTime;
+        float t;
+        while (curTime < length)
+        {
+            Debug.Log("Interpolating pp effects");
+            curTime = Time.time - initTime;
+            Debug.Log("curTime: " + curTime);
+            t = curTime / length;
+            bloom.intensity.Interp(dayBloom.intensity, nightBloom.intensity, t);
+            bloom.color.Interp(dayBloom.color, nightBloom.color, t);
+            bloom.threshold.Interp(dayBloom.threshold, nightBloom.threshold, t);
+
+            vignette.intensity.Interp(dayVignette.intensity, nightVignette.intensity, t);
+            vignette.color.Interp(dayVignette.color, nightVignette.color, t);
+            vignette.smoothness.Interp(dayVignette.smoothness, nightVignette.smoothness, t);
+
+            light.color = Vector4.Lerp(daylight, moonlight, t);
+
+            yield return stepDelay;
+        }
+
+        Debug.Log("Finished interpolating pp effects");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,13 @@ public class Player : MonoBehaviour
 
     public float stamina;
 
+    public bool drainStamina;
+    [Tooltip("Amount drained per drain tick")]
+    public float stamDrain = 0.001f;
+    [Tooltip("Seconds between stam drain ticks")]
+    public float drainStep = 1f;
+    WaitForSeconds drainWait;
+
     private void Awake()
     {
         if(instance != null)
@@ -20,9 +27,30 @@ public class Player : MonoBehaviour
         {
             instance = this;
             inventory = new Inventory();
+
+            drainWait = new WaitForSeconds(drainStep);
         }
     }
 
+    public void StartStaminaDrain()
+    {
+        StartCoroutine(StaminaDrain());
+    }
+
+
+    IEnumerator StaminaDrain()
+    {
+        drainStamina = true;
+
+        while(drainStamina && stamina > 0)
+        {
+            stamina -= stamDrain;
+            yield return drainWait;
+        }
+
+        Debug.Log("DEADZ");
+        drainStamina = false;
+    }
 
 }
 
