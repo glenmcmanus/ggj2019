9fd95d655cbe1a511b614917389995190272ae6b
Michael Demone <m_d_emone@hotmail.com>
2019-01-25 23:34:14 -0800

    made click hander and nav mesh stuff

---
+++ Assets/Code/PlayerMovement/ClickToMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickToMove : MonoBehaviour
+{
+    public Camera MainCamera;
+    public float MaxCastDistance = 200;
+
+    public UnityEventVector3 OnClickHit;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                OnClickHit.Invoke(hit.point);
+            }
+
+        }
+    }
+}
---
+++ Assets/Code/PlayerMovement/PlayerLerper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLerper : MonoBehaviour
+{
+    public float MoveSpeed = 0.5f;
+
+    public void StartLerping()
+    {
+
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
