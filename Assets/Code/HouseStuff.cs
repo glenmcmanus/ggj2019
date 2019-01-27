95853d232ee08d7c02e449f3b749f3945ee20feb
Michael Demone <m_d_emone@hotmail.com>
2019-01-26 21:15:23 -0800

    Made a fucking huge scritable object

---
+++ Assets/Code/HouseStuff.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "HouseValues")]
+public class HouseStuff : ScriptableObject
+{
+    [Header("Minimums & Defines")]
+    public int minWoodForFirePerDay = 24;
+    public int daysUntilPassBlockedAtBeginning = 20;
+
+
+    [Header("Player")]
+    public int minFoodForPersonPerDay = 3;
+    public float healthRegenPerFoodForPerson = 33.3f;
+    public float staminaLossPerMissingWood = 1;
+
+    [Header("Dog")]
+    public float healthRegenPerFoodForDog = 33.3f;
+    public int diseaseOneMedicineCures = 5;
+    public int diseaseAdvancesPerDay = 10;
+    public float healthLossPerWoodGoneForDog = 1;
+    public float diseaseGrowthPerWoodGone = 2;
+    public float damageOneDiseaseDoesToDogPerNight = 0.5f;
+
+    public int minMedicinePerDayForDogPerDay { get { return diseaseAdvancesPerDay / diseaseOneMedicineCures; } }
+
+
+    [Header("Runtime Variables")]
+    public int woodForSled = 0;
+    public int woodForFire = 0;
+    public int foodForDog = 0;
+    public int foodForYou = 0;
+    public int medicineForDog = 0;
+
+    public float dogDisease = 100;
+    public float dogHealth = 25;
+
+    public int daysUntilPassBlocked = 20;
+
+    public void StartHouse()
+    {
+        Player.instance.StopAllCoroutines();
+    }
+
+    public void Submit()
+    {
+        // Deal with player
+        Player.instance.inventory.food -= foodForYou;
+        Player.instance.inventory.wood -= woodForFire;
+
+        float nextDayStamina = foodForYou * healthRegenPerFoodForPerson;
+        nextDayStamina -= (minWoodForFirePerDay - woodForFire) * staminaLossPerMissingWood;
+
+        // Deal with sled building
+        Player.instance.inventory.wood -= woodForSled;
+        Sled.instance.GiveWood(woodForSled);
+
+        // Deal with pup
+        Player.instance.inventory.food -= foodForDog;
+        Player.instance.inventory.medicine -= medicineForDog;
+
+        float healthForDogTonight = foodForDog * healthRegenPerFoodForDog;
+        healthForDogTonight -= (minWoodForFirePerDay - woodForFire) * healthLossPerWoodGoneForDog;
+
+        dogDisease += diseaseGrowthPerWoodGone;
+        dogDisease -= medicineForDog * diseaseOneMedicineCures;
+
+        healthForDogTonight -= dogDisease * damageOneDiseaseDoesToDogPerNight;
+        dogHealth += healthForDogTonight;
+        dogHealth = Mathf.Max(dogHealth, 100);
+
+        daysUntilPassBlocked -= 1;
+
+        if(dogHealth <= 0)
+        {
+            TheDogFuckingDies();
+        }
+    }
+
+    public void TheDogFuckingDies()
+    {
+
+    }
+}
---
+++ Assets/Code/Sled.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sled : MonoBehaviour
+{
+    public static Sled instance;
+
+    public Sprite[] StageSprites;
+
+    public int woodNeededForStage1 = 10;
+    public int woodNeededForStage2 = 15;
+    public int woodNeededForStage3 = 25;
+    public int woodNeededForStage4 = 25;
+    public int woodNeededForStage5 = 25;
+
+    public int totalWood = 0;
+    public bool complete = false;
+
+    public SpriteRenderer spriteRenderer;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void GiveWood(int amount)
+    {
+        totalWood += amount;
+        if(totalWood > woodNeededForStage5)
+        {
+            spriteRenderer.sprite = StageSprites[4];
+            complete = true;
+        }
+        else if (totalWood > woodNeededForStage4)
+        {
+            spriteRenderer.sprite = StageSprites[3];
+        }
+        else if (totalWood > woodNeededForStage3)
+        {
+            spriteRenderer.sprite = StageSprites[2];
+        }
+        else if (totalWood > woodNeededForStage2)
+        {
+            spriteRenderer.sprite = StageSprites[1];
+        }
+        else if(totalWood > woodNeededForStage1)
+        {
+            spriteRenderer.sprite = StageSprites[0];
+        }
+        else
+        {
+            spriteRenderer.sprite = null;
+        }
+    }
+}
